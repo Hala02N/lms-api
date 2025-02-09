9928693b561efbf87870be6de6ce340b3af6d026
Hala Alnadeem <hala.alnadeem@hotmail.com>
2025-02-09 09:39:34 +0300

    refactor: add throw error statements in the methods signature in UserService.java interface

--- src/main/java/com/example/services/UserService.java
+++ src/main/java/com/example/services/UserService.java
@@ -2,13 +2,14 @@ package com.example.services;
 
 import com.example.entities.UserEntity;
 import com.example.requests.CreateUserRequest;
+import javassist.tools.web.BadHttpRequest;
 
 import java.util.List;
 
 public interface UserService {
-    public UserEntity getUserById(String studentId);
+    public UserEntity getUserById(String studentId) throws BadHttpRequest;
 
-    UserEntity getUserByEmail(String email);
+    UserEntity getUserByEmail(String email) throws BadHttpRequest;
     List<UserEntity> getAllUsers();
     UserEntity createUser(CreateUserRequest student);
     UserEntity updateUser(UserEntity student);
