7fe3f740846657ff532d8ed2c2b1a6c8d575bc8d
Hala Alnadeem <hala.alnadeem@hotmail.com>
2025-02-09 09:54:25 +0300

    refactor: update namings of params and vars for better readability.

--- src/main/java/com/example/controllers/UserController.java
+++ src/main/java/com/example/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.example.controllers;
 
+import com.example.exceptions.AuthException;
 import com.example.requests.LoginUserRequest;
 import com.example.entities.UserEntity;
 import com.example.events.CreateUserEvent;
@@ -11,9 +12,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,49 +26,27 @@ public class UserController {
     private UserService userService;
     @Autowired
     private MessageProducer messageProducer;
-    @Autowired
-    private JWTUtil jwtUtil;
-    @Autowired
-    private AuthenticationManager authManager;
 
     @PostMapping("/enroll")
-    public ResponseEntity<?> enrollUser(@RequestBody @Valid CreateUserRequest studentRequest, BindingResult bindingResult, HttpServletRequest request) {
+    public ResponseEntity<?> enrollUser(@RequestBody @Valid CreateUserRequest userRequest, BindingResult bindingResult, HttpServletRequest request) {
         String email = (String)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        System.out.println(email);
-        UserEntity createdStudent = userService.createUser(studentRequest);
+        System.out.println("ADMIN EMAIL: " + email);
+        UserEntity createdUser = userService.createUser(userRequest);
 
         // Create a user event and populate it with the created user id and major
         CreateUserEvent userEvent = new CreateUserEvent();
-        userEvent.setId(createdStudent.getId());
-        userEvent.setMajor(createdStudent.getMajor());
+        userEvent.setId(createdUser.getId());
+        userEvent.setMajor(createdUser.getMajor());
 
         // After user creation, send the event to a specific topic in kafka broker
         messageProducer.sendMessage("lms-topic2", "Student", userEvent);
-
-        // Generate and send a token for the newly created user
-        String token = jwtUtil.generateJWToken(studentRequest.getEmail());
-        return ResponseEntity.ok(Collections.singletonMap("jwt_token", token));
+        return ResponseEntity.ok(createdUser);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> loginHandler(@RequestBody LoginUserRequest body) {
-        try {
-          //  UsernamePasswordAuthenticationToken authInputToken = new UsernamePasswordAuthenticationToken(body.getEmail(), body.getPassword());
-          //  authManager.authenticate(authInputToken);
-            // authenticate user
-            String token = userService.authenticateUser(body.getEmail(), body.getPassword());
+    public ResponseEntity<?> loginHandler(@RequestBody LoginUserRequest request) {
+            // Authenticate user
+            String token = userService.authenticateUser(request.getEmail(), request.getPassword());
             return ResponseEntity.ok(Collections.singletonMap("jwt_token", token));
-        } catch (UsernameNotFoundException e) {
-            throw new RuntimeException("Invalid Login Credentials");
-        }
     }
 }
-/*
-        if (bindingResult.hasErrors()) {
-            Map<String, String> errors = new HashMap<>();
-            bindingResult.getFieldErrors().forEach(error -> {
-                errors.put(error.getField(), error.getDefaultMessage());
-            });
-            return ResponseEntity.badRequest().body(errors);
-        }
-*/
